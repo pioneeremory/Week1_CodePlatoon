45f8095c1bddc80f25624764a3776cf5c556892f
logan <blacksuitphoto@gmail.com>
2025-11-08 11:02:13 -0800

    updates

---
+++ factorialChallenge.js
@@ -0,0 +1,15 @@
+// Factorial Challenge
+// In this exercise you are going to create a function that takes a number parameter and returns the factorial of it.
+// Factorial has a very specific definition. Learn more here. As an overview though, a factorial is the result of multiplying a sequence 
+// of descending numbers down to 0. Factorials are only defined for non-negative integer numbers. This definition includes zero. 
+// Though 0! is equal to 1, so treat it as an edge case.
+let num1 = Number(prompt('What number do you need the factor of?'));
+
+function findFactor(num1) {
+    let count = 1
+    for (let i = num1; i = 0; i--) {
+        factor = num1 * i
+    }
+    return factor
+}
+
--- practice.js
+++ practice.js
@@ -1,1 +1,38 @@
-#this is week1 intermediate js
+//this is week1 intermediate js
+
+//CONSTANTS you cannot change the value of a constant
+const num = 1;
+num2 = 42;
+// results in ERROR
+
+//VARIABLES you can change the value of a variable
+
+let num1 = 1;
+num2 = 42;
+
+// FUNCTIONS
+
+makeFullName(firstName, lastName);
+
+function makeFullName(firstName,lastName) {
+    return firstName + ' ' + lastName;
+}
+
+// run file with node [file.name]
+//Note that you were able to call the function (seemingly) before it was declared. 
+// This is because of a feature in JS called 'hoisting', which pulls all function definitions to the top of the file 
+// before any other code is ran.
+
+//String Interpolation
+const firstName = "Tom";
+console.log(`My first name is ${firstName}`);
+
+// To find the data type of something
+typeof 5; //number
+typeof true; //bool
+
+// MATH
+
+Math.floor(5.23); //5 (rounds a number to the nearest whole)
+
+
