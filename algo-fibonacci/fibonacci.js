63c09c80346e11c1ce2f3775017c683fd3e87092
logan <blacksuitphoto@gmail.com>
2025-11-10 17:47:57 -0800

    fibonacci assignment

--- algo-fibonacci/fibonacci.js
+++ algo-fibonacci/fibonacci.js
@@ -1,5 +1,34 @@
+let nextFib = 0;
+
 function fibonacci(num) {
-  return 0;
+  if (num === 0) {
+    return 0;
+  }
+  if (num === 1) {
+    return 1;
+  }
+  // if (num < 2) {
+  //   return num;
+  // }
+
+  let a = 0;
+  let b = 1;
+
+  for (let i = 2; i <= num; i++) {
+    nextFib = a + b;
+
+    a = b;
+    b = nextFib;
+  }
+
+  console.log(`The ${num}th number of the Fibonacci Sequence is ${nextFib}`)
+  return b;
 }
 
+fibonacci(20);
+
 module.exports = fibonacci;
+
+// Your challenge today is to write an algorithm that, given a number n, return the n-th Fibonacci Number. 
+// Keep in mind that the first two numbers in the fibonacci sequence are always [0,1]
+//F(n) = F(n-1) + F(n-2)
