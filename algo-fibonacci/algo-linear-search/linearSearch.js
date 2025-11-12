4228dea9f6715ba055cedc484cfd8f2a3ea75027
logan <blacksuitphoto@gmail.com>
2025-11-12 08:12:02 -0800

    algo fib update

--- algo-fibonacci/algo-linear-search/linearSearch.js
+++ algo-fibonacci/algo-linear-search/linearSearch.js
@@ -1,6 +1,18 @@
+let searchList;
+
 function linearSearch(searchTerm, arr) {
+  searchList = searchTerm.split("");
+  console.log(searchList);
+
+  for (let i = 0; i > arr.length; i++) {
+    if (arr[i] === searchList)
+      return arr[i];
+  }
+
   return undefined;
 }
+
+linearSearch("a", searchList)
 
 function globalLinearSearch(searchTerm, arr) {
   return [];
